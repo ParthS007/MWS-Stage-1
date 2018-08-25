0ab84c45feb498218b9212e8748e815ea52caa95
Parth Shandilya <parth1989shandilya@gmail.com>
2018-08-26 01:36:08 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,18 +12,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
   registerServiceWorker();
 });
 
-/*Register a service worker for caching static and dynamic assets.*/
-registerServiceWorker = () => {
-  if (!navigator.serviceWorker) {
-    return;
-  }
-  navigator.serviceWorker.register('../service-worker.js').then(() => {
-    console.log('Service worker registered successfully!');
-  }).catch((error) => {
-    console.log('Error while registering service worker:', error);
-  });
-}
-
 /*Fetch all neighborhoods and set their HTML.*/
 fetchNeighborhoods = () => {
   DBHelper.fetchNeighborhoods((error, neighborhoods) => {
